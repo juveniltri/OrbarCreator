485f0a7f387817c4b2d91f6127c018a21df613f9
Victor Ramos <victorramospoveda@gmail.com>
2024-03-13 11:56:46 +0100

    TextArea y añadir a card

--- src/components/CartaOrbat.svelte
+++ src/components/CartaOrbat.svelte
@@ -3,13 +3,15 @@
 	import Separator from '$lib/components/ui/separator/separator.svelte';
 	import Persona from './Persona.svelte';
 
-	const escuadra = `RACOON 1-2
-Líder de Equipo (R12)-Diablo
-Fusilero (R12)-Aguja
-Fusilero (R12)-Rylod
-Fusilero AT (R12)-Fernanrl
-Sanitario (R12)-JMartin`;
-	const escuadraNombre = 'RACOON 1-2';
+	export let texto = '';
+
+	let lineas = texto.split('\n');
+	const escuadraNombre = lineas[0];
+	let miembros = lineas.slice(1).map(linea => {
+		let [rol, nombre] = linea.split('-');
+		return { rol: rol.trim(),
+				 nombre: nombre.trim() };
+	});
 </script>
 
 <Card.Root class="w-[700px]">
@@ -19,8 +21,9 @@ Sanitario (R12)-JMartin`;
 	<Separator class="mb-5" />
 	<Card.Content>
 		<ol class="list-decimal">
-			<Persona nombre="Proplayer" rol="Medico" />
-			<Persona nombre="Proplayer" rol="Medico" />
+			{#each miembros as {nombre, rol}}
+				<Persona {nombre} {rol}/>
+			{/each}
 		</ol>
 	</Card.Content>
 </Card.Root>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,10 +1,24 @@
 <script>
 	import CartaOrbat from '../components/CartaOrbat.svelte';
+	let textoEscuadra = '';
+	let mostrarCarta = false;
 </script>
 
-<div class="p-5">
-	<CartaOrbat></CartaOrbat>
+<textarea bind:value={textoEscuadra} class="w-full h-32 p-2"/>
+
+<div class="flex space-x-2 mt-2">
+    <button on:click={() => { mostrarCarta = true; }} class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
+        Generar Carta
+    </button>
+    <button on:click={() => { textoEscuadra = ''; mostrarCarta = false; }} class="bg-gray-500 hover:bg-gray-700 text-white font-bold py-2 px-4 rounded">
+        Limpiar
+    </button>
 </div>
 
+{#if mostrarCarta }
+<div class="p-5">
+	<CartaOrbat texto={textoEscuadra}></CartaOrbat>
+</div>
+{/if}
 <style>
 </style>
