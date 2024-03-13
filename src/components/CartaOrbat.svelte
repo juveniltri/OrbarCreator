9e5d3833e76eea4155f271a5d138aa939ed3a658
Latons <Latons@gormukka.xyz>
2024-03-13 02:29:31 +0100

    Subcomponente Persona

--- src/components/CartaOrbat.svelte
+++ src/components/CartaOrbat.svelte
@@ -1,78 +1,23 @@
 <script>
 	import * as Card from '$lib/components/ui/card/index.js';
 	import Separator from '$lib/components/ui/separator/separator.svelte';
-	import Button from '$lib/components/ui/button/button.svelte';
-	let esRojo = false;
-	let esAzul = false;
-	let esOro = false;
-	let esVerde = false;
-	function setRojo(event) {
-		esAzul = false;
-		esVerde = false;
-		esOro = false;
-		esRojo = !esRojo;
-	}
-	function setAzul(event) {
-		esRojo = false;
-		esVerde = false;
-		esOro = false;
-		esAzul = !esAzul;
-	}
-	function setOro(event) {
-		esRojo = false;
-		esVerde = false;
-		esAzul = false;
-		esOro = !esOro;
-	}
-	function setVerde(event) {
-		esRojo = false;
-		esOro = false;
-		esAzul = false;
-		esVerde = !esVerde;
-	}
+	import Persona from './Persona.svelte';
+
+	const escuadra = `RACOON 1-2
+Líder de Equipo (R12)-Diablo
+Fusilero (R12)-Aguja
+Fusilero (R12)-Rylod
+Fusilero AT (R12)-Fernanrl
+Sanitario (R12)-JMartin`;
+	const escuadraNombre = 'RACOON 1-2';
 </script>
 
 <Card.Root class="w-[700px]">
 	<Card.Header>
-		<Card.Title>Alpha 1-1</Card.Title>
+		<Card.Title>{escuadraNombre}</Card.Title>
 	</Card.Header>
 	<Separator class="mb-5" />
 	<Card.Content>
-		<div
-			class="flex items-center"
-			class:bg-red-300={esRojo}
-			class:bg-blue-300={esAzul}
-			class:bg-yellow-300={esOro}
-			class:bg-green-300={esVerde}
-		>
-			<p class="mr-24">1. snakemarenostrum</p>
-			<div>
-				<Button class="w-24 bg-red-500" on:click={setRojo}>Rojo</Button>
-				<Button class="w-24 bg-blue-500" on:click={setAzul}>Azul</Button>
-				<Button class="w-24 bg-yellow-500" on:click={setOro}>Oro</Button>
-				<Button class="w-24 bg-green-500" on:click={setVerde}>Verde</Button>
-			</div>
-		</div>
-		<Separator class="my-5" />
-		<div class="flex items-center">
-			<p class="mr-24">1. snakemarenostrum</p>
-			<div>
-				<Button class="w-24 bg-red-500">Rojo</Button>
-				<Button class="w-24 bg-blue-500">Azul</Button>
-				<Button class="w-24 bg-yellow-500">Oro</Button>
-				<Button class="w-24 bg-green-500">Verde</Button>
-			</div>
-		</div>
-		<Separator class="my-5" />
-		<div class="flex items-center">
-			<p class="mr-24">1. snakemarenostrum</p>
-			<div>
-				<Button class="w-24 bg-red-500">Rojo</Button>
-				<Button class="w-24 bg-blue-500">Azul</Button>
-				<Button class="w-24 bg-yellow-500">Oro</Button>
-				<Button class="w-24 bg-green-500">Verde</Button>
-			</div>
-		</div>
-		<Separator class="my-5" />
+		<Persona nombre="Proplayer" rol="Medico" />
 	</Card.Content>
 </Card.Root>
---
+++ src/components/Persona.svelte
@@ -0,0 +1,53 @@
+<script>
+	import Button from '$lib/components/ui/button/button.svelte';
+	import Separator from '$lib/components/ui/separator/separator.svelte';
+
+	export let nombre;
+	export let rol;
+
+	let esRojo = false;
+	let esAzul = false;
+	let esOro = false;
+	let esVerde = false;
+	function setRojo(event) {
+		esAzul = false;
+		esVerde = false;
+		esOro = false;
+		esRojo = !esRojo;
+	}
+	function setAzul(event) {
+		esRojo = false;
+		esVerde = false;
+		esOro = false;
+		esAzul = !esAzul;
+	}
+	function setOro(event) {
+		esRojo = false;
+		esVerde = false;
+		esAzul = false;
+		esOro = !esOro;
+	}
+	function setVerde(event) {
+		esRojo = false;
+		esOro = false;
+		esAzul = false;
+		esVerde = !esVerde;
+	}
+</script>
+
+<div
+	class="flex items-center"
+	class:bg-red-300={esRojo}
+	class:bg-blue-300={esAzul}
+	class:bg-yellow-300={esOro}
+	class:bg-green-300={esVerde}
+>
+	<p class="mr-24">1. {rol}: {nombre}</p>
+	<div>
+		<Button class="w-24 bg-red-500" on:click={setRojo}>Rojo</Button>
+		<Button class="w-24 bg-blue-500" on:click={setAzul}>Azul</Button>
+		<Button class="w-24 bg-yellow-500" on:click={setOro}>Oro</Button>
+		<Button class="w-24 bg-green-500" on:click={setVerde}>Verde</Button>
+	</div>
+</div>
+<Separator class="my-5" />
