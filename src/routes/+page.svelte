f4d97bc33beec90841bb04051edfdee9fb19c42b
Latons <Latons@gormukka.xyz>
2024-03-13 12:19:58 +0100

    retoques

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -4,29 +4,41 @@
 	let escuadras = [];
 
 	function generarCartas() {
-		escuadras = textoEscuadra.trim().split('\n\n').map(escuadras => {
-			return escuadras.trim();
-		});		
+		escuadras = textoEscuadra
+			.trim()
+			.split('\n\n')
+			.map((escuadras) => {
+				return escuadras.trim();
+			});
 	}
 </script>
 
-<textarea bind:value={textoEscuadra} class="w-full h-32 p-2"/>
+<textarea bind:value={textoEscuadra} class="h-32 w-full p-2" />
 
-<div class="flex space-x-2 mt-2">
-    <button on:click={generarCartas} class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
-        Generar Carta
-    </button>
-    <button on:click={() => { textoEscuadra = ''}} class="bg-gray-500 hover:bg-gray-700 text-white font-bold py-2 px-4 rounded">
-        Limpiar
-    </button>
+<div class="m-2 flex space-x-2">
+	<button
+		on:click={generarCartas}
+		class="rounded bg-blue-500 px-4 py-2 font-bold text-white hover:bg-blue-700"
+	>
+		Generar Carta
+	</button>
+	<button
+		on:click={() => {
+			textoEscuadra = '';
+		}}
+		class="rounded bg-gray-500 px-4 py-2 font-bold text-white hover:bg-gray-700"
+	>
+		Limpiar
+	</button>
 </div>
 
 {#if escuadras.length > 0}
-	<div class="p-5">
-		{#each escuadras as equipoTexto(equipoTexto)}
-			<CartaOrbat {equipoTexto}></CartaOrbat>			
+	<div class="mx-2 grid grid-cols-3 gap-y-3">
+		{#each escuadras as equipoTexto (equipoTexto)}
+			<CartaOrbat {equipoTexto}></CartaOrbat>
 		{/each}
 	</div>
 {/if}
+
 <style>
 </style>
