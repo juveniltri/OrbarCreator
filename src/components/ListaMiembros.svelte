8db905624621432ea1d97fe9603e47d0af15fc5c
Latons <Latons@gormukka.xyz>
2024-03-15 14:02:59 +0100

    Mejorado el CSS y lógica de separador

--- src/components/ListaMiembros.svelte
+++ src/components/ListaMiembros.svelte
@@ -11,7 +11,7 @@
 
 	let colorSeleccionado = {};
 	function setColor(color, index) {
-		console.log("Color: " + color + " y index: " + index);
+		console.log('Color: ' + color + ' y index: ' + index);
 		colorSeleccionado[index] = colorSeleccionado[index] === color ? '' : color;
 		miembros[index].color = colorSeleccionado[index];
 	}
@@ -87,11 +87,14 @@
 					<Button class="w-4/12 bg-red-500" on:click={() => setColor('rojo', index)}>Rojo</Button>
 					<Button class="w-4/12 bg-blue-500" on:click={() => setColor('azul', index)}>Azul</Button>
 					<Button class="w-4/12 bg-yellow-500" on:click={() => setColor('oro', index)}>Oro</Button>
-					<Button class="w-4/12 bg-green-500" on:click={() => setColor('verde', index)}>Verde</Button
+					<Button class="w-4/12 bg-green-500" on:click={() => setColor('verde', index)}
+						>Verde</Button
 					>
 				</div>
 			</div>
-			<Separator class="my-5" />
+			{#if index !== miembros.indexOf(miembros[miembros.length - 1])}
+				<Separator class="my-5" />
+			{/if}
 		</li>
 	{/each}
 </ol>
